c8051390b01c1a28aaba7bb3fa73392fe5c3ee24
Faisal Al-Thuwaini <faisal.yalthuwaini@gmail.com>
2023-07-28 21:30:50 +0300

    editing links and logo text

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = `سطور`;
+export const SITE_TITLE = `سُطور`;
 export const SITE_DESCRIPTION = 'Welcome to my blog!';
